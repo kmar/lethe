1781640c5ae70fe9bd5943f1d93989bda70d13c4
kmar <k_mar@seznam.cz>
2020-03-09 12:53:32 -0700

    disable silly offsetof warning in gcc/clang

--- src/Lethe/Lethe.h
+++ src/Lethe/Lethe.h
@@ -3,3 +3,9 @@
 #include "Script/ScriptInit.h"
 #include "Script/ScriptEngine.h"
 #include "Script/Utils/FormatStr.h"
+
+#if LETHE_COMPILER_GCC || LETHE_COMPILER_CLANG
+// disable silly warning about offsetof + nonstd layout,
+// I've yet to meet a programmer who ever used virtual inheritance
+#	pragma GCC diagnostic ignored "-Winvalid-offsetof"
+#endif
