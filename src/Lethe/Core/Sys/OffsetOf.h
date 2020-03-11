886ba8d7c54901d5ab580905527c9d099cb34984
kmar <k_mar@seznam.cz>
2020-03-11 21:46:21 +0100

    drop constexpr as local var is C++14 extension hoping the optimizer will remove the local instance

--- src/Lethe/Core/Sys/OffsetOf.h
+++ src/Lethe/Core/Sys/OffsetOf.h
@@ -10,7 +10,7 @@ namespace lethe
 template <typename T1, typename T2>
 struct offset_of_impl
 {
-	static inline constexpr int offset(T1 T2::*member)
+	static inline int offset(T1 T2::*member)
 	{
 		T2 obj;
 		return (int)size_t(&(obj.*member)) -
@@ -19,7 +19,7 @@ struct offset_of_impl
 };
 
 template <typename T1, typename T2>
-inline constexpr int offset_of(T1 T2::*member)
+inline int offset_of(T1 T2::*member)
 {
 	return offset_of_impl<T1, T2>::offset(member);
 }
